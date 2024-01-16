c60c0a60b7ec112a747d01dba5645efa1114424d
FSW2005 <felix.wikstrom@elev.ga.lbs.se>
2024-01-16 21:16:26 +0100

    M8r
    Modifiera bara FelixMovement scripten

--- kranken/Assets/Code/FelixMovement.cs
+++ kranken/Assets/Code/FelixMovement.cs
@@ -10,10 +10,13 @@ public class FelixMovement : MonoBehaviour
     public float crouchHeight = 0.5f;
     public float standingHeight = 2.0f;
     public float sprintSpeedMultiplier = 2.0f;
+    public float maxStamina = 100.0f;
+    public float staminaRegenerationRate = 10.0f;
 
     private CharacterController characterController;
     private bool isSprinting = false;
     private bool isCrouching = false;
+    private float currentStamina;
 
     private Camera playerCamera;
     private bool isRotatingCamera = false;
@@ -23,12 +26,14 @@ public class FelixMovement : MonoBehaviour
     {
         characterController = GetComponent<CharacterController>();
         playerCamera = Camera.main;
+        currentStamina = maxStamina;
     }
 
     void Update()
     {
         HandleMovement();
         HandleCameraRotation();
+        HandleActions();
     }
 
     void HandleMovement()
@@ -41,7 +46,7 @@ public class FelixMovement : MonoBehaviour
 
         if (moveDirection != Vector3.zero)
         {
-            Quaternion newRotation = Quaternion.LookRotation(moveDirection);
+            Quaternion newRotation = Quaternion.LookRotation(cameraForward);
             transform.rotation = Quaternion.RotateTowards(transform.rotation, newRotation, rotationSpeed * Time.deltaTime);
         }
 
@@ -51,9 +56,16 @@ public class FelixMovement : MonoBehaviour
         {
             speed /= 2.0f;
         }
-        else if (isSprinting && Input.GetKey(KeyCode.LeftShift))
+        else if (isSprinting && currentStamina > 0)
         {
             speed *= sprintSpeedMultiplier;
+            currentStamina -= Time.deltaTime;
+        }
+        else if (currentStamina < maxStamina)
+        {
+            // Regenerate stamina passively when not sprinting
+            currentStamina += staminaRegenerationRate * Time.deltaTime;
+            currentStamina = Mathf.Clamp(currentStamina, 0f, maxStamina);
         }
 
         Vector3 movement = moveDirection * speed * Time.deltaTime;
@@ -78,16 +90,62 @@ public class FelixMovement : MonoBehaviour
             Vector2 deltaMousePosition = (Vector2)Input.mousePosition - lastMousePosition;
             lastMousePosition = Input.mousePosition;
 
-            float rotationX = deltaMousePosition.y * rotationSpeed * Time.deltaTime;
-            float rotationY = -deltaMousePosition.x * rotationSpeed * Time.deltaTime;
+            float rotationX = -deltaMousePosition.y * rotationSpeed * Time.deltaTime;
+            float rotationY = deltaMousePosition.x * rotationSpeed * Time.deltaTime;
 
             playerCamera.transform.Rotate(Vector3.up * rotationY);
             playerCamera.transform.Rotate(Vector3.right * rotationX);
 
             // Optionally, you can limit the camera rotation in the X-axis to prevent flipping
             Vector3 currentRotation = playerCamera.transform.eulerAngles;
-            currentRotation.x = Mathf.Clamp(currentRotation.x, 0.0f, 90.0f);
+            currentRotation.x = Mathf.Clamp(currentRotation.x, -90.0f, 90.0f);
             playerCamera.transform.eulerAngles = currentRotation;
         }
+    }
+
+    void HandleActions()
+    {
+        // Sprint action (Hold "Left Shift" key)
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            StartSprint();
+        }
+        else
+        {
+            StopSprint();
+        }
+
+        // Crouch action (Hold "Left Ctrl" key)
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            ToggleCrouch();
+        }
+    }
+
+    void ToggleCrouch()
+    {
+        isCrouching = !isCrouching;
+
+        if (isCrouching)
+        {
+            characterController.height = crouchHeight;
+        }
+        else
+        {
+            characterController.height = standingHeight;
+        }
+    }
+
+    void StartSprint()
+    {
+        if (!isCrouching && currentStamina > 0)
+        {
+            isSprinting = true;
+        }
+    }
+
+    void StopSprint()
+    {
+        isSprinting = false;
     }
 }
