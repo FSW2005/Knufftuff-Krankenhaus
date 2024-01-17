d5cf2a4a49785c5e7d9ced1287b274f340ffb49d
FSW2005 <felix.wikstrom@elev.ga.lbs.se>
2024-01-17 09:48:46 +0100

    MUUUUU
    ~]--(-_-)--[~
                 |   |
                <  <
    Uppdatera "Interact" och "Door" scripterna.
    Nya 3dModeler blev importerade också!

---
+++ kranken/Assets/Code/Interact.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interact : MonoBehaviour
+{
+    // This method will be called when the player interacts with the object
+    public virtual void Interactable()
+    {
+        Debug.Log("Interacting with: " + gameObject.name);
+        // Add your interaction logic here
+    }
+
+    void Update()
+    {
+        // Check for player input (e.g., pressing the "E" key) to trigger interaction
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            // Raycast to detect the interactable object in front of the player
+            RaycastHit hit;
+            if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, 3.0f))
+            {
+                Interact interactable = hit.collider.GetComponent<Interact>();
+                if (interactable != null)
+                {
+                    // Call the Interact method of the interactable object
+                    interactable.Interactable();
+                }
+            }
+        }
+    }
+}
